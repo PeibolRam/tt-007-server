7c2703fdd5eb8d5719b8979a38228b9c1f4da7cc
Pablo Ramirez <pablo@dreamsengine.io>
2020-02-28 12:21:14 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+
+const  { User } = require('./models/user')
+const { auth } = require('./middleware/auth')
+
+require('dotenv').config();
+
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err) => {
+    if(err) return err
+    console.log("Conectado a MongoDB")
+})
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cookieParser())
+
+
+//Rutas user
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body)
+    user.save((err, doc) => {
+        if(err) return res.json({success: false, err})
+        res.status(200).json({
+            success: true,
+            userdata: doc
+        })
+    })
+})
+
+app.post('/api/users/login', (req, res) => {
+    User.findOne({'email': req.body.email}, (err,user) => {
+        if(!user) return res.json({loginSuccess: false, message: 'Auth fallida, email no encontrado'})
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) return res.json({loginSuccess: false, message: "Contraseña incorrecta"})
+
+            user.generateToken((err, user)=> {
+                if(err) return res.status(400).send(err)
+                res.cookie('b_auth', user.token).status(200).json(
+                    {loginSuccess: true}
+                )
+            })
+        })
+    })
+})
+
+app.get('/api/users/auth', auth, (req, res) => {
+    res.status(200).json({
+        // isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role
+    })
+})
+
+app.get('/api/user/logout', auth, (req, res) => {
+    User.findOneAndUpdate(
+        {_id: req.user._id},
+        {token: ''},
+        (err, doc) => {
+            if(err) return res.json({success: false, err})
+            return res.status(200).json({
+                success: true
+            })
+        }
+    )
+})
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${process.env.PORT}`)
+})
